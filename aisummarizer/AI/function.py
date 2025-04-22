bee6dba4ea7fcf1fbb7b54738f74db6ac0a213e9
MakaRonish <ronishmakaju12345@gmail.com>
2025-04-21 23:53:58 -0400

    css

--- aisummarizer/AI/function.py
+++ aisummarizer/AI/function.py
@@ -15,7 +15,7 @@ def summarizerAI(name):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=[
-            f"Summarize the book {name} .I want a detailed yet concise summary that includes: The main plot or narrative structure Key characters and their development Major themes and messages Important events or turning points The author's purpose or style (if relevant) Please keep the summary engaging and easy to understand, like explaining it to someone who hasn’t read the book but wants to know what it’s about and why it matters."
+            f"Summarize the book {name} .I want a detailed yet concise summary that includes: The main plot or narrative structure Key characters and their development Major themes and messages Important events or turning points The author's purpose or style (if relevant) Please keep the summary engaging and easy to understand, like explaining it to someone who hasn’t read the book but wants to know what it’s about and why it matters.Also write in proper format like the space paragraph and all dont use unnecesarry symbols make it as professional and write atleast 1500 words"
         ],
     )
     summarry = response.text
--- aisummarizer/AI/views.py
+++ aisummarizer/AI/views.py
@@ -13,6 +13,8 @@ def landingpage(request):
         data = form.save(commit=False)
         data.save()
         summary = summarizerAI(data.Context)
+        summary = summary.replace("*", "")
+        print(summary)
 
     context = {"DocumentForm": form, "summary": summary}
     return render(request, "AI/landing.html", context=context)
