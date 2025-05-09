47c0aacb27448293f445b0129c39db27e2b53c8a
MakaRonish <ronishmakaju12345@gmail.com>
2025-05-08 21:49:30 -0400

    signup

--- aisummarizer/userapp/form.py
+++ aisummarizer/userapp/form.py
@@ -7,3 +7,13 @@ class CustomUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["email", "username", "password1", "password2"]
+        labels = {"email": "", "username": "", "password1": "", "password2": ""}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].label = ""
+        self.fields["password2"].label = ""
+        self.fields["username"].widget.attrs["placeholder"] = "Username"
+        self.fields["email"].widget.attrs["placeholder"] = "Email"
+        self.fields["password1"].widget.attrs["placeholder"] = "Password"
+        self.fields["password2"].widget.attrs["placeholder"] = "Confirm Password"
